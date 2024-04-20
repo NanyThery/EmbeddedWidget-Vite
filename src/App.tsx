dd782ee9750c55ad871798e0ffcfba1d831ec3fa
NanyThery <nadinethery@gmail.com>
2024-04-20 10:26:43 +0200

    add wrapper style in app to have a visual reference for widget behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import Widget from "./components/Widget/Widget";
+import "../src/styles/global.css";
+import styles from "./App.module.css";
 
+// This initializes an app with the webcomponent, but Vite is only exporting the standalone Widget component
 function App() {
-  return <Widget price={12300} />;
+  return (
+    <div className={styles.wrapper}>
+      <Widget price={12300} />
+    </div>
+  );
 }
 
 export default App;
