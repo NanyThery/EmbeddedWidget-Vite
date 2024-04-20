09659d13ce1c051bc3c1982099eb90d3caff2144
NanyThery <nadinethery@gmail.com>
2024-04-20 15:26:20 +0200

    add input for testing visually the output of the widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import Widget from "./components/Widget/Widget";
 import "../src/styles/global.css";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 // This initializes an app with the webcomponent, but Vite is only exporting the standalone Widget component
 function App() {
+  const [price, setPrice] = useState("2000");
   return (
+    /*Simulates what may come from the parent app*/
     <div className={styles.wrapper}>
-      <Widget price={12300} />
+      <label>Price</label>
+      <input
+        type="text"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+
+      <Widget price={price} />
     </div>
   );
 }
